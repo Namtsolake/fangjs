cd8f2839101d1729ce1dd83967d40949b88e0e56
ty <1539683580@qq.com>
2017-07-16 14:58:53 +0800

    ty xg1

--- src/Component/Detail/index.js
+++ src/Component/Detail/index.js
@@ -5,48 +5,18 @@ import {connect} from "react-redux";
 class Detail extends Component{
 	constructor(props){
 		super(props);
-		this.state={
-			filminfo:null
-		}
+
 	}
 
-	componentDidMount(){
-		console.log(this.props.match.params);
-		axios.get(`/v4/api/film/${this.props.match.params.kerwinID}`).then(res=>{
-			console.log(res.data);
-			this.setState({
-				filminfo:res.data.data.film
-			})
-			this.props.change(res.data.data.film.name)
-		})
-	}
+	
 
 	render(){
 		return(
 			<div>
-			{
-				this.state.filminfo?
-				(
-				<div>
-			<h3>{this.state.filminfo.name}</h3>
-			<img src={this.state.filminfo.poster.origin}/>
-			</div>
-			):null
-		}
+			
 			</div>
 			)
 	}
 }
 
-export default connect(
-	null,
-	{
-		change:(name)=>{
-			console.log(name);
-			return{
-				type:"KERWINCHANGE",
-				kerwininfo:name
-			}
-		}
-	}
-	)(Detail);
+export default Detail;
--- src/Component/Investmentproject/index.js
+++ src/Component/Investmentproject/index.js
@@ -6,68 +6,20 @@ import InfiniteScroll from 'react-infinite-scroller';
 class Investmentproject extends Component{
 	constructor(props){
 		super(props);
-		this.state={
-			playinglist:[],
-			total:0,
-			currentpage:0,
-			hasMore:true
-		}
+
 	}
 
-componentWillUnmount() {
-	// window.removeEventListener("scroll",this.myscrollfunc)
-}
+
 
 	render(){
 		return(
 			<div id="investmentproject">
-			<ul>
-			<InfiniteScroll
-		    pageStart={0}
-		    loadMore={this.handleLoadeMore.bind(this)}
-		    hasMore={this.state.hasMore}
-		    initialLoad={false}
-		    loader={<div className="loader">Loading ...</div>}>
-			{
-				this.state.playinglist.map((item,index)=>
-					<li key={item.id} onClick={()=>{
-						this.props.history.push("/home")
-					}}>
-					<img src={item.poster.origin}/>
-					<h3>{item.name}</h3>
-					<p>{item.intro}</p>
-					</li>
-					)
-			}
-			</InfiniteScroll>
-			</ul>
+			
 			</div>
 			)
 	}
 
-	handleLoadeMore(){
-		console.log("请求数据");
-
-		var current=++this.state.currentpage;
-		if(current>this.state.total){
-			this.setState({
-				hasMore:false
-			})
-			return;
-		}
-		axios.get("/v4/api/film/now-playing",{
-		params:{
-			page:current,
-			count:7
-		}
-	}).then(res=>{
-		console.log(res.data);
-		this.setState({
-			playinglist:[...this.state.playinglist,...res.data.data.films],
-		
-		})
-	})
-	}
+	
 }
 
 export default Investmentproject; 
