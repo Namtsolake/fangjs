c4f87f9a31d5736878d87ceff98b3f492b8b96b2
yunhan <2670893251@qq.com>
2017-07-16 21:23:40 +0800

    yunhan-sha

--- src/Component/Home/index.js
+++ src/Component/Home/index.js
@@ -2,6 +2,9 @@ import React,{Component} from "react";
 import "./index.scss";
 import axios from "axios";
 import ReactSwipe from "react-swipe";
+import {
+	NavLink
+}from "react-router-dom";
 class Home extends Component{
 	constructor(prop){
 		super(prop);
@@ -11,7 +14,31 @@ class Home extends Component{
 	render(){
 		return(
 			<div id="home">
-			
+			<div className="all">
+				
+				<div className="lunbo">
+					
+				</div>
+				<div className="gonggao">
+				<p>最新公告：<span>房金贷+部分借款提前还款公告</span></p>
+				</div>
+				<div className="xiangqing">
+				<NavLink to="/detail" activeClassName="suiyi"></NavLink>
+				</div>
+				<a href="javascript:;" className="tiao"><section className="dong">
+					<canvas id="canvas" width="500" height="500"></canvas>
+					<span className="up">预期年化收益率</span>
+					<span className="down">5.90%</span>
+				</section></a>
+				<ul>  
+					<li>1000元起投</li>
+					<li>期限58天</li>
+					<li>一次性还本付息</li>
+				</ul>
+				<div className="dianji">
+					<NavLink to="/detail" activeClassName="suiyi">立即投资</NavLink>
+				</div>
+			</div>
 			</div>
 			)
 
--- src/Component/More/index.js
+++ src/Component/More/index.js
@@ -1,5 +1,8 @@
 import React,{Component} from "react";
 import "./index.scss";
+import {
+	NavLink
+}from "react-router-dom";
 
 class More extends Component{
 	constructor(props){
@@ -8,8 +11,37 @@ class More extends Component{
 
 	render(){
 		return(
-			<div>
-			More
+			<div id="more">
+				<div className="all">
+					<div className="zho">
+						<div className="datu"></div>
+						<div className="yi">
+							客服热线<span>10101818 ></span>	
+							<p></p>
+						</div>
+						<div className="er">
+							官方微信<span>房金所官微</span>	
+							<p></p>
+						</div>
+						<div className="san">
+							官方微博<span>房金所官博</span>	
+							<p></p>
+						</div>
+						<div className="si">
+							官方网站<span>www.fangjs.com</span>	
+							<p></p>
+						</div>
+						<div className="wu">
+						<NavLink to="/home" activeClassName="suiyi">帮助中心</NavLink><span><a href="javascript:;">></a></span>	
+							<p></p>
+						</div>
+						<div className="liu">
+						<NavLink to="/home" activeClassName="suiyi">房金所介绍</NavLink><span><a href="javascript:;">></a></span>	
+							<p></p>
+						</div>
+						<div className="qi"></div>
+					</div>	
+				</div>
 			</div>
 			)
 	}
